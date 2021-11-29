57607b951458df4260de32d42514a25f0f2027bc
Ben-Phung <90236372+Ben-Phung@users.noreply.github.com>
2021-11-29 16:40:47 -0500

    Add files via upload

---
+++ main2.cpp
@@ -0,0 +1,38 @@
+// /*
+//  *  UCF COP3330 Fall 2021 Assignment 5 Solution
+//  *  Copyright 2021 Ben Phung
+//  */
+
+// #include <iostream>
+// #include "std_lib_facilities.h"
+
+// using namespace std;
+
+// int main() {
+//   string operation;
+//   double op1, op2;
+//   double ans;
+
+//   cout << "Enter operation and two operands: ";
+//   cin >> operation >> op1 >> op2;
+//   if (operation == "+" || operation == "plus")
+//   {
+//     cout << op1 <<" plus "<< op2 <<" is equal to "<<(op1+op2);
+//   }
+//   else if (operation == "-" || operation == "minus")
+//   {
+//     cout << op1 <<" minus "<< op2 <<" is equal to "<<(op1-op2);
+//   }
+//   else if (operation == "*" || operation == "mul")
+//   {
+//     cout << op1 <<" times "<< op2 <<" is equal to "<<(op1*op2);
+//   }
+//   else if (operation == "/" || operation == "div")
+//   {
+//     cout << op1 <<" divided by "<< op2 <<" is equal to "<<(op1/op2);
+//   }
+//   else
+//   {
+//     cout<<"Invalid input";
+//   }
+// }
